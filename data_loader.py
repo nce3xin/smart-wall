18392f75130a3c814f09fac1224600400eb25967
zhangxin <zhangx@dsp.ac.cn>
2019-08-20 16:02:44 +0800

    5-fold stratified cross validation

--- data_loader.py
+++ data_loader.py
@@ -1,8 +1,26 @@
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
 import numpy as np
 
 def data_split(root,X_file_name,y_file_name):
     X=np.load(root + '/' + X_file_name)
     y=np.load(root + '/' + y_file_name)
-    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,stratify=y)
-    return X_train,X_test,y_train,y_test
+    skf = StratifiedKFold(n_splits=5)
+    X_train_k_fold=[]
+    y_train_k_fold=[]
+    X_test_k_fold=[]
+    y_test_k_fold=[]
+    for train_index,test_index in skf.split(X,y):
+        #print("TRAIN:", train_index, "TEST:", test_index)
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        X_train_k_fold.append(X_train)
+        y_train_k_fold.append(y_train)
+        X_test_k_fold.append(X_test)
+        y_test_k_fold.append(y_test)
+
+    #X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,stratify=y)
+    return X_train_k_fold,X_test_k_fold,y_train_k_fold,y_test_k_fold
+
+if __name__=='__main__':
+    data_split('data/npy','X.npy','y.npy')
--- train.py
+++ train.py
@@ -8,26 +8,33 @@ if __name__=='__main__':
     X_file_name='X.npy'
     y_file_name='y.npy'
 
-    X_train,X_test,y_train,y_test=data_split(root,X_file_name,y_file_name)
+    X_train_k_fold,X_test_k_fold,y_train_k_fold,y_test_k_fold = data_split(root,X_file_name,y_file_name)
 
-    X_train=np.expand_dims(X_train,axis=3)
-    X_test=np.expand_dims(X_test,axis=3)
-    '''
-    X_train=X_train.reshape((X_train.shape[0],80,50,1))
-    X_test=X_test.reshape((X_test.shape[0],80,50,1))
-    '''
-    y_train=keras.utils.to_categorical(y_train,num_classes=2)
-    y_test=keras.utils.to_categorical(y_test,num_classes=2)
-    
-    img_channels=1
-    img_rows=10
-    img_cols=400
-    num_classes=2
-    model=resnet.ResnetBuilder.build_resnet_34((img_channels,img_rows,img_cols),num_classes)
-    model.compile(loss='categorical_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
-    model.fit(X_train,y_train,batch_size=32,epochs=10)
-    score=model.evaluate(X_test,y_test,batch_size=32)
-    
-    print(score)
+    for i in range(len(X_train_k_fold)):
+        X_train=X_train_k_fold[i]
+        X_test=X_test_k_fold[i]
+        y_train=y_train_k_fold[i]
+        y_test=y_test_k_fold[i]
+
+        X_train=np.expand_dims(X_train,axis=3)
+        X_test=np.expand_dims(X_test,axis=3)
+        '''
+        X_train=X_train.reshape((X_train.shape[0],80,50,1))
+        X_test=X_test.reshape((X_test.shape[0],80,50,1))
+        '''
+        y_train=keras.utils.to_categorical(y_train,num_classes=2)
+        y_test=keras.utils.to_categorical(y_test,num_classes=2)
+        
+        img_channels=1
+        img_rows=10
+        img_cols=400
+        num_classes=2
+        model=resnet.ResnetBuilder.build_resnet_34((img_channels,img_rows,img_cols),num_classes)
+        model.compile(loss='categorical_crossentropy',
+                optimizer='adam',
+                metrics=['accuracy'])
+        print('{} fold -----------------------------------------------------'.format(i+1))
+        model.fit(X_train,y_train,batch_size=32,epochs=1)
+        score=model.evaluate(X_test,y_test,batch_size=32)
+        
+        print(score)
